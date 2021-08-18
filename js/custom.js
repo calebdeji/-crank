a44575f4a8b0ee54df7ae35e9a6f50931aa0d995
calebdeji <calebdeji06@gmail.com>
2021-08-18 08:24:50 +0100

    changes

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', startCarouselTimer);
+
+function startCarouselTimer() {
+	const firstButton = document.getElementById('carousel_1');
+	const secondButton = document.getElementById('carousel_2');
+
+	let isFirstCarouselActive = true;
+
+	window.setInterval(() => {
+		if (isFirstCarouselActive) {
+			secondButton.click();
+		} else {
+			firstButton.click();
+		}
+
+		isFirstCarouselActive = !isFirstCarouselActive;
+	}, 10000);
+}
